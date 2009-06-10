09c0082560780087426af826f30978d18c8793ec
donga.spirit <donga.spirit@fb971198-4629-11de-89ec-3d9467717a5a>
2009-06-10 18:39:27 +0000

    layout and form detail

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 // change the following paths if necessary
-$yii = 'D:/projects/yii-1.0.5.r1018/framework/yii.php';
+$yii = '../yii/framework/yii.php';
 $config = dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following line when in production mode
